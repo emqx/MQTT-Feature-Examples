cb8085a5f3a7e8b7ffd15191fb67c873b5562c01
zhouzb <zhouzb@emqx.io>
2023-08-08 17:16:08 +0800

    chore: better variable name

--- examples/mqttv5/message_expiry_interval.py
+++ examples/mqttv5/message_expiry_interval.py
@@ -12,70 +12,70 @@ def publish(client, topic, payload, properties):
     print("[Publish] Topic: %s, Payload: %s" % (topic, payload))
     client.publish(topic, payload, properties = properties)
 
-callback = utils.Callbacks()
-callback2 = utils.Callbacks()
+a_callback = utils.Callbacks()
+b_callback = utils.Callbacks()
 
-# aclient as publisher
+# a_client as publisher
 aclientid = utils.random_clientid()
-aclient = mqtt.Client(aclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
-callback.register(aclient)
+a_client = mqtt.Client(aclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
+a_callback.register(a_client)
 
-# bclient as subsriber
+# b_client as subsriber
 bclientid = utils.random_clientid()
 connect_properties = Properties(PacketTypes.CONNECT)
 connect_properties.SessionExpiryInterval = 300
-bclient = mqtt.Client(bclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
-callback2.register(bclient)
+b_client = mqtt.Client(bclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
+b_callback.register(b_client)
 
-aclient.connect(host = HOST, port = PORT, clean_start = True)
-aclient.loop_start()
-response = callback.wait_connected()
+a_client.connect(host = HOST, port = PORT, clean_start = True)
+a_client.loop_start()
+response = a_callback.wait_connected()
 
-bclient.connect(host = HOST, port = PORT, clean_start = True, properties = connect_properties)
-bclient.loop_start()
-response = callback2.wait_connected()
+b_client.connect(host = HOST, port = PORT, clean_start = True, properties = connect_properties)
+b_client.loop_start()
+response = b_callback.wait_connected()
 
 topic = bclientid + "/demo"
-bclient.subscribe(topic, qos = 2)
-response = callback2.wait_subscribed()
-bclient.disconnect()
-callback2.wait_disconnected()
-bclient.loop_stop()
-callback2.clear()
+b_client.subscribe(topic, qos = 2)
+response = b_callback.wait_subscribed()
+b_client.disconnect()
+b_callback.wait_disconnected()
+b_client.loop_stop()
+b_callback.clear()
 
 # Publish messages with the expiry interval after the subscriber goes offline
 publish_properties = Properties(PacketTypes.PUBLISH)
 publish_properties.MessageExpiryInterval = 5
-publish(aclient, topic, "Expiry Interval is 5 seconds", publish_properties)
+publish(a_client, topic, "Expiry Interval is 5 seconds", publish_properties)
 
 publish_properties.MessageExpiryInterval = 60
-publish(aclient, topic, "Expiry Interval is 60 seconds", publish_properties)
+publish(a_client, topic, "Expiry Interval is 60 seconds", publish_properties)
 
 # The subscriber waits 6 seconds before reconnecting
 time.sleep(6)
-bclient = mqtt.Client(bclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
-callback2.register(bclient)
+b_client = mqtt.Client(bclientid.encode("utf-8"), protocol = mqtt.MQTTv5)
+b_callback.register(b_client)
 
 connect_properties = Properties(PacketTypes.CONNECT)
 connect_properties.SessionExpiryInterval = 0
-bclient.connect(host = HOST, port = PORT, clean_start = False, properties = connect_properties)
-bclient.loop_start()
-response = callback2.wait_connected()
+b_client.connect(host = HOST, port = PORT, clean_start = False, properties = connect_properties)
+b_client.loop_start()
+response = b_callback.wait_connected()
 
-utils.waitfor(callback2.messages, 2, 1)
+utils.waitfor(b_callback.messages, 2, 1)
 
-if len(callback2.messages) == 1:
-    msg = callback2.messages[0]["message"]
+if len(b_callback.messages) == 1:
+    msg = b_callback.messages[0]["message"]
     print("[Received] Topic: %s, Payload: %s, Remaining expiry interval: %d" %
           (msg.topic, msg.payload.decode("utf-8"), msg.properties.MessageExpiryInterval))
 else:
     print("Unexpected result")
 
 # Disconnect
-aclient.disconnect()
-callback.wait_disconnected()
-aclient.loop_stop()
+a_client.disconnect()
+a_callback.wait_disconnected()
+a_client.loop_stop()
 
-bclient.disconnect()
-callback2.wait_disconnected()
-bclient.loop_stop()
+b_client.disconnect()
+b_callback.wait_disconnected()
+b_client.loop_stop()
